9027b4fe4a854259fe7317faa593981835800a32
thesatmannn <seanom12@gmail.com>
2023-07-20 10:20:20 -0500

    add styling to container and add font

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,8 @@
-// default project//
-document.querySelector
+// eslint-disable-next-line import/prefer-default-export
+export function defaultProject() {
+    const content = document.getElementsByClassName("content");
+    const header = document.createElement("h1");
+    header.innerText = "Projects";
+   
 
+}
